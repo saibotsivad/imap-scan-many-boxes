3dbae07afe5cd245d026e7aa7e68c2b18a1d77f2
Tobias Davis <accounts@davistobias.com>
2017-06-08 23:38:12 -0500

    copy stuff over

---
+++ index.js
@@ -0,0 +1,61 @@
+const EventEmitter = require('events')
+const PQueue = require('p-queue')
+const scanBox = require('imap-scan-box')
+
+// It is a limitation of the IMAP protocol, I believe,
+// or at least of the `imap` module, that fetching
+// messages from multiple boxes at the same time is
+// simply not possible. In any case, using the `imap`
+// module means that the sequence number starts drifting
+// and other similarly confusing issues arise when
+// multiple scans are allowed to occur at the same time.
+const concurrencyLimit = 1
+
+const defaultRange = '1:*'
+const defaultFetch = {
+	bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+	struct: true
+}
+
+module.exports = ({ imap, boxes = [], range = defaultRange, fetch = defaultFetch }) => {
+	const emitter = new EventEmitter()
+	const queue = new PQueue({ concurrency: concurrencyLimit })
+
+	boxes.forEach(boxName => {
+		queue.add(() => new Promise((resolve, reject) => {
+			const scanner = scanBox(imap, boxName, range, fetch)
+
+			scanner.once('end', resolve)
+
+			scanner.once('error', ({ action, error }) => {
+				emitter('error', {
+					action,
+					error,
+					box: boxName
+				})
+				reject()
+			})
+
+			scanner.on('opened', box => emitter.emit('opened', box))
+			scanner.on('closed', box => emitter.emit('closed', box))
+
+			scanner.on('message', ({ sequenceNumber, stream }) => {
+				emitter.emit({
+					sequenceNumber,
+					box: boxName,
+					stream
+				})
+			})
+		}))
+	})
+
+	queue
+		.onEmpty()
+		.then(() => emitter.emit('end'))
+		.catch(error => {
+			emitter.emit('error', { action: 'queue.onEmpty', error })
+			emitter.emit('end')
+		})
+
+	return emitter
+}
